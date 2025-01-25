159abc8c0edfff60f27d6aa6fd69011622dd0d0c
Echo Yin <echo_ybl@hotmail.com>
2025-01-25 16:15:36 -0700

    Add a guide panel

--- Assets/Scripts/UI/MainMenuPanel.cs
+++ Assets/Scripts/UI/MainMenuPanel.cs
@@ -3,25 +3,44 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
+
 namespace UI
 {
         public class MainMenuPanel : MonoBehaviour
         {
+            [Header("Buttons")]
             [SerializeField] private Button startButton;
             [SerializeField] private Button guideButton;
             [SerializeField] private Button quitButton;
+            [SerializeField] private Button backToMainButton;
 
+            [Header("Sub Panels")] 
+            [SerializeField] private GameObject buttonContainer;
+            [SerializeField] private GameObject guidePanel;
+            
             private void Start()
             {
-                startButton.onClick.AddListener(SceneController.LoadGameScene);
-                quitButton.onClick.AddListener(SceneController.QuitGame);
-                // startButton.onClick.AddListener(ButtonClick);
-                // startButton.interactable = true;
+                InitializePanels();
+                AssignListeners();
             }
 
-            public static void ButtonClick()
+            private void InitializePanels()
             {
-                Debug.Log("Button clicked.");
+                buttonContainer.SetActive(true);
+                guidePanel.SetActive(false);
+            }
+            private void AssignListeners()
+            {
+                startButton.onClick.AddListener(SceneController.LoadGameScene);
+                guideButton.onClick.AddListener(DisplayGuide);
+                quitButton.onClick.AddListener(SceneController.QuitGame);
+                backToMainButton.onClick.AddListener(InitializePanels);
+            }
+
+            private void DisplayGuide()
+            {
+                buttonContainer.SetActive(false);
+                guidePanel.SetActive(true);
             }
         }
 }
